703f46b90d15a37473dd740acbc5e4221194d20f
Molnár József <mojo12984@gmail.com>
2023-03-01 14:08:25 +0100

    created monster class

---
+++ Monster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_tower_climber
+{
+    internal class Monster
+    {
+    }
+}
