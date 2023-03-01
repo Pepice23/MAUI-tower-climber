e6063c122b717d1a072840cf2871cb885aea56d5
Molnár József <mojo12984@gmail.com>
2023-03-01 12:22:16 +0100

    create Player and Weapon class

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI_tower_climber
+{
+    internal class Player
+    {
+        public bool Visible;
+        public int Level;
+        public int Money;
+        public int PerClickDamage;
+        public int PerSecondDamage;
+        public int DamageMultiplier;
+        public int Floor;
+        public int MonsterCount;
+        public int CurrentXP;
+        public int XPToNextLevel;
+        public string Avatar;
+
+        public Player()
+        {
+            Visible = true;
+            Level = 1;
+            Money = 0;
+            PerClickDamage = 10;
+            PerSecondDamage = 10;
+            DamageMultiplier = 1;
+            Floor = 1;
+            MonsterCount = 1;
+            CurrentXP = 0;
+            XPToNextLevel = 100;
+            Avatar = "hero1.jpeg";
+        }
+    }
+}
