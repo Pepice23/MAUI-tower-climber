5fcdac682999dd53fb63622d1818b1fa3a5d42b7
Molnár József <mojo12984@gmail.com>
2023-03-02 14:24:19 +0100

    delete platforms i wont use

--- Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace MAUI_tower_climber;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
